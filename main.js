a508094ce2653d870384b3e3fb4519558c7f1cc3
cosimogiove <cosimo18497@gmail.com>
2023-11-28 15:29:45 +0100

    feat:milestone1

--- main.js
+++ main.js
@@ -4,6 +4,23 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
+        lista:[
+            {
+                testo:"andare al parco",
+                fatto:true,
+            },
+            {
+                testo:"mangiare il gelato",
+                fatto:true,
+            },
+            {
+                testo:"fare i compiti",
+                fatto:true,
+            },
+
+        ]
     }
+  },
+  methods:{
   }
 }).mount('#app');
